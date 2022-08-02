021c88bd9764d058f21d4e80adc599717a47dc47
Ian Hwang <44934582+meen813@users.noreply.github.com>
2022-08-02 16:20:19 -0700

    Add files via upload

---
+++ assignments/Cores/CafeBusinessLogic/CafeUtil.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class CafeUtil {
+
+    //getStreakGoal method
+    public int getStreakGoal( int weeks ){
+        int sum = 0;
+        int i = 1;
+        while( i <= weeks){
+            sum = sum + i ;
+            i++;
+        }
+        return sum;
+    }
+    
+    //getOrderTotal method
+    public double getOrderTotal(double[] prices){
+        double total = 0.0;
+        for(int i = 0; i <prices.length; i++){
+            total += prices[i];
+        }
+        
+        return total;
+    }
+    
+    //displayMenu method
+    public void displayMenu(ArrayList<String> menuItems){
+        for( int i = 0; i < menuItems.size(); i++){
+            System.out.println( i + " " + menuItems.get(i) );
+        }
+    }
+
+    //addCustomer method
+    ArrayList<String> customerList = new ArrayList<String>();
+    public void addCustomer(ArrayList<String> customers){
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.println("Hello " + userName + "!");
+        System.out.println("There are " + customerList.size() + " people in front of you");
+        customerList.add(userName);
+        System.out.println(customerList);;
+
+    }
+}
