b9c622fea8a0a8dec66f3556a11c05acf3651726
Ian Hwang <44934582+meen813@users.noreply.github.com>
2022-08-03 00:55:15 -0700

    Add files via upload

---
+++ assignments/practice/OrdersAndItems/Order.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Order{
+    public String name;
+    public Double total = 0.0;
+    public Boolean ready = false;
+    
+    
+
+    ArrayList<String> items = new ArrayList<String>();
+    
+
+}
